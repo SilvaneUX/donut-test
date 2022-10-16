7e5d2c4d86626f8046f5655bc3f2278884015b94
Valentino Harpa <67551202+bukanvalen@users.noreply.github.com>
2022-10-16 07:45:57 -0700

    Add new contributors, add Rust folder, and rename Python folder

---
+++ Programing Language/Rust/testcase_list.rs
@@ -0,0 +1,35 @@
+// Answer file for the activity section on `1.2.2.1. Testcase: List`
+// https://doc.rust-lang.org/rust-by-example/hello/print/print_display/testcase_list.html
+
+use std::fmt; // Import the `fmt` module.
+
+// Define a structure named `List` containing a `Vec`.
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Extract the value using tuple indexing,
+        // and create a reference to `vec`.
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        // Iterate over `v` in `vec` while enumerating the iteration
+        // count in `count`.
+        for (count, v) in vec.iter().enumerate() {
+            // For every element except the first, add a comma.
+            // Use the ? operator to return on errors.
+            if count != 0 { write!(f, ", ")?; }
+            // ANSWER: Add `count` to print the index of each element
+            write!(f, "{}: {}", count, v)?;
+        }
+
+        // Close the opened bracket and return a fmt::Result value.
+        write!(f, "]")
+    }
+}
+
+fn main() {
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+}

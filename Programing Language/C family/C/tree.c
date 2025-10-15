0351f30ad6ec618e534ab3dc910493456837c0cc
SilvaneUX <46640918+SilvaneUX@users.noreply.github.com>
2025-10-15 16:58:02 +0700

    asdas
    sadas

--- Programing Language/C family/C/tree.c
+++ Programing Language/C family/C/tree.c
@@ -1,56 +1,179 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
-    int data;
-    struct Node *left;
-    struct Node *right;
+struct pohon{
+   char data;
+   struct pohon *kiri;
+   struct pohon *kanan;
 };
+struct pohon *root = NULL;
+void masukdata(int data);
+struct pohon* cari(int data);
+void preorder(struct pohon* root);
+void inorder(struct pohon* root);
+void postorder(struct pohon* root);
 
-struct Node* createNode(int value) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = value;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    return newNode;
+int main(){
+   int i, jum = 0, menu = 0;
+   char carik;
+
+   puts("PRAKTIKUM TREE SEARCHING GAGAS");
+   printf("\nmau memasukkan berapa data ? : ");
+   scanf("%d", &jum);
+   char masuk[jum];
+   fflush(stdin);
+
+   for(i = 0; i < jum; i++){
+    fflush(stdin);
+    printf("masukkan data ke %d : ", i);
+    scanf("%c", &masuk[i]);
+    fflush(stdin);
+   }
+
+   for(i = 0; i < jum; i++)
+      masukdata(masuk[i]);
+
+   do{
+       puts("\nMenu TREE");
+       puts("1.PreOrder Tree");
+       puts("2.InOrder Tree");
+       puts("3.PostOrder Tree");
+       puts("4.ALL Tree");
+       puts("5.Cari Tree");
+       puts("6.Exit");
+       printf("masukkan pilihan anda : ");
+       scanf("%d", &menu);
+       fflush(stdin);
+
+
+       if(menu == 1){
+        printf("Preorder Tree: ");
+        preorder(root);
+        printf("\n");
+       }
+
+       else if(menu == 2){
+         printf("Inorder Tree: ");
+         inorder(root);
+         printf("\n");
+       }
+
+       else if(menu == 3){
+        printf("Postorder Tree: ");
+        postorder(root);
+        printf("\n");
+       }
+
+       else if(menu == 4){
+        printf("Preorder Tree\t: ");
+        preorder(root);
+
+        printf("\nInorder Tree\t: ");
+        inorder(root);
+
+        printf("\nPostorder Tree\t: ");
+        postorder(root);
+        printf("\n");
+       }
+
+       else if(menu == 5){
+           printf("\nmasukkan data yang ingin di cari (T untuk keluar) : ");
+           scanf("%c", &carik);
+           fflush(stdin);
+
+           struct pohon* temp = cari(carik);
+           if(temp != NULL) {
+              printf("[%c] Ketemu.", carik);
+              printf("\n");
+           }else{
+              printf("[%c] Tidak ketemu :((.\n", carik);
+           }
+       }else{
+        puts("Arigatu Gonzaimas");
+        exit(0);
+       }
+   }while(1);
 }
 
-void inorder(struct Node* root) {
-    if (root != NULL) {
-        inorder(root->left);
-        printf("%d ", root->data);
-        inorder(root->right);
-    }
+void masukdata(int data) {
+   struct pohon *tempNode = (struct pohon*) malloc(sizeof(struct pohon));
+   struct pohon *current;
+   struct pohon *parent;
+
+   tempNode->data = data;
+   tempNode->kiri = NULL;
+   tempNode->kanan = NULL;
+
+   if(root == NULL) {
+      root = tempNode;
+   }else {
+      current = root;
+      parent = NULL;
+
+      while(1) {
+         parent = current;
+
+         if(data < parent->data) {
+            current = current->kiri;
+
+            if(current == NULL) {
+               parent->kiri = tempNode;
+               return;
+            }
+         }
+         else {
+            current = current->kanan;
+
+
+            if(current == NULL) {
+               parent->kanan = tempNode;
+               return;
+            }
+         }
+      }
+   }
 }
 
-void preorder(struct Node* root) {
-    if (root != NULL) {
-        printf("%d ", root->data);
-        preorder(root->left);
-        preorder(root->right);
-    }
+struct pohon* cari(int data) {
+   struct pohon *current = root;
+   printf("Pencarian Tree : ");
+
+   while(current->data != data) {
+
+      if(current->data > data) {
+         current = current->kiri;
+      }
+      else {
+         current = current->kanan;
+      }
+
+      if(current == NULL) {
+         return NULL;
+      }
+   }
+   return current;
 }
 
-void postorder(struct Node* root) {
-    if (root != NULL) {
-        postorder(root->left);
-        postorder(root->right);
-        printf("%d ", root->data);
-    }
+void preorder(struct pohon* root) {
+   if(root != NULL) {
+      printf("%c ",root->data);
+      preorder(root->kiri);
+      preorder(root->kanan);
+   }
 }
 
-int main() {
-    struct Node* root = createNode(1);
-    root->left = createNode(2);
-    root->right = createNode(3);
-    root->left->left = createNode(4);
-    root->left->right = createNode(5);
+void inorder(struct pohon* root) {
+   if(root != NULL) {
+      inorder(root->kiri);
+      printf("%c ",root->data);
+      inorder(root->kanan);
+   }
+}
 
-    printf("Inorder traversal: ");
-    inorder(root);
-    printf("\nPreorder traversal: ");
-    preorder(root);
-    printf("\nPostorder traversal: ");
-    postorder(root);
-    return 0;
+void postorder(struct pohon* root) {
+   if(root != NULL) {
+      postorder(root->kiri);
+      postorder(root->kanan);
+      printf("%c ", root->data);
+   }
 }
